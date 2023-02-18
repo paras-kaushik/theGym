47ea275e989ed4dd368777467df0661ee4810d0c
Paras Kaushik <parkaush1@WKMZT5811CC5.global.publicisgroupe.net>
2023-02-18 12:26:13 +0530

    hard work

---
+++ src/firebase/config.js
@@ -0,0 +1,26 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAX8ZOaQJTptcLUlPD-jo7KfCsC-P1a_LU",
+  authDomain: "thegym-2493c.firebaseapp.com",
+  projectId: "thegym-2493c",
+  storageBucket: "thegym-2493c.appspot.com",
+  messagingSenderId: "566189469998",
+  appId: "1:566189469998:web:9cc88d4d160fd2e05a0abf",
+  measurementId: "G-QK3Q1W1L7T"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+// init services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+// timestamp
+const timestamp = firebase.firestore.Timestamp
+
+export { projectFirestore, projectAuth, timestamp }
---
+++ src/hooks/useCollection.js
@@ -0,0 +1,44 @@
+import { useEffect, useState, useRef } from "react"
+import { projectFirestore } from "../firebase/config"
+// This hook is all about subscribing to data from a realtime collection
+// this only reads whereas - the userFirestore modifies the data
+export const useCollection = (collection, _query, _orderBy) => {
+  const [documents, setDocuments] = useState(null)
+  const [error, setError] = useState(null)
+
+  // if we don't use a ref --> infinite loop in useEffect
+  // _query is an array and is "different" on every function call
+  const query = useRef(_query).current
+  const orderBy = useRef(_orderBy).current;
+
+  useEffect(() => {
+    let ref = projectFirestore.collection(collection)
+
+    if (query) {
+      ref = ref.where(...query)
+    }
+    if (orderBy) {
+      ref = ref.orderBy(...orderBy)
+    }
+
+    const unsubscribe = ref.onSnapshot(snapshot => {
+      let results = []
+      snapshot.docs.forEach(doc => {
+        results.push({...doc.data(), id: doc.id})
+      });
+      // above id is not user id bu the document id, we need to store this if we want to delete this in future
+      // update state
+      setDocuments(results)
+      setError(null)
+    }, error => {
+      console.log(error)
+      setError('could not fetch the data')
+    })
+
+    // unsubscribe on unmount
+    return () => unsubscribe()
+
+  }, [collection, query, orderBy])
+
+  return { documents, error }
+}
