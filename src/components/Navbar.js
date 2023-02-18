8a989005754f3aa852875a4971af4713eea56ff4
Paras Kaushik <parkaush1@WKMZT5811CC5.global.publicisgroupe.net>
2023-02-18 13:14:25 +0530

    om shree ganesh

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,8 +12,8 @@ export default function Navbar() {
   return (
     <nav className={styles.navbar}>
       <ul>
-        <li className={styles.title}>myMoneyApp</li>
-        
+        <li className={styles.title}>AMAZINO FITNESS</li>
+
         {!user && (
           <>
             <li><Link to="/login">Login</Link></li>
